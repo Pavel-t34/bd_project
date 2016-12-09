5a9c01b8970a0809790b6a212ff8a282501311fa
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:19:50 +0800

    Create scanavto.php

---
+++ forms/scanavto.php
@@ -0,0 +1,14 @@
+<form action="<?= $url ?>" method="post">
+
+
+  <label>Номер</label><br>
+  <input type="text" name="number" value=""><br>
+  <br>
+
+  <label>Владелец</label><br>
+  <input type="text" name="vin" value=""><br>
+  <br>
+  
+  
+  <button type="submit">Поиск</button>
+</form>

b298708500131cef75eda547d0b95430087b83b7
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:18:45 +0800

    Create regins.php

---
+++ forms/regins.php
@@ -0,0 +1,17 @@
+<form action="<?= $url ?>" method="post">
+
+
+  <label>Имя</label><br>
+  <input type="text" name="name" value=""><br>
+  <br>
+  
+  <label>Фамилия</label><br>
+  <input type="text" name="surname" value=""><br>
+  <br>
+
+  <label>Звание</label><br>
+  <input type="text" name="rang" value=""><br>
+  <br>
+  
+  <button type="submit">Сохранить</button>
+</form>

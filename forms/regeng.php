2bbf13cea248b68111741eed524ee38c2c89680e
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:18:26 +0800

    Create regeng.php

---
+++ forms/regeng.php
@@ -0,0 +1,18 @@
+<form action="<?= $url ?>" method="post">
+
+
+  <label>Модель</label><br>
+  <input type="text" name="model" value=""><br>
+  <br>
+  
+  <label>Мощность</label><br>
+  <input type="text" name="power" value=""><br>
+  <br>
+
+  <label>vin</label><br>
+  <input type="text" name="vin" value=""><br>
+  <br>
+  
+  
+  <button type="submit">Сохранить</button>
+</form>

84332e000e000be49133262070a1f8f01c0ff382
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:17:58 +0800

    Create regavto.php

---
+++ forms/regavto.php
@@ -0,0 +1,35 @@
+<form action="<?= $url ?>" method="post">
+
+
+  <label>Марка</label><br>
+  <input type="text" name="brand" value=""><br>
+  <br>
+  
+  <label>Модель</label><br>
+  <input type="text" name="model" value=""><br>
+  <br>
+
+  <label>двигатель</label><br>
+  <input type="text" name="engine" value=""><br>
+  <br>
+  
+  <label>Рег.номер</label><br>
+  <input type="text" name="number" value=""><br>
+  <br>
+  
+  <label>Владелец</label><br>
+  <input type="text" name="owner" value=""><br>
+  <br>
+  
+  <label>год</label><br>
+  <input type="text" name="year" value=""><br>
+  <br>
+  
+  <label>vin</label><br>
+  <input type="text" name="vin" value=""><br>
+
+  
+  
+  
+  <button type="submit">Сохранить</button>
+</form>

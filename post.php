9b2fc45d7571bee910ddd8d86d2468e5169c5a43
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:04:18 +0800

    Create post.php

---
+++ post.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<body>
+  <a href="/regprot.php">Регистрация нарушения</a>
+  <br>
+  <a href="/scanavto.php">Поиск водителя</a>
+  <br>
+  <a href="/regeng.php">Поиск автомобиля</a>
+  <br>
+</body>
+</html>

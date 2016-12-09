431f1023cf22e99c9cf0dd85c9c29b60c91ced74
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:00:03 +0800

    Create secret.php

---
+++ secret.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+<body>
+  <a href="/regavto.php">Регистрация автомобилей</a>
+  <br>
+  <a href="/regvod.php">Регистрация водителей</a>
+  <br>
+  <a href="/regeng.php">Регистрация двигателя</a>
+  <br>
+  <a href="/regins.php">Регистрация инспектора</a>
+
+</body>
+</html>

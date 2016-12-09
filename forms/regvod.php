4f2598c049a130b4ddc83510543156258ed84342
Pavel-t34 <Paflik12@gmail.com>
2016-12-09 22:19:31 +0800

    Create regvod.php

---
+++ forms/regvod.php
@@ -0,0 +1,25 @@
+<form action="<?= $url ?>" method="post">
+
+
+  <label>Имя</label><br>
+  <input type="text" name="name" value=""><br>
+  <br>
+  
+  <label>Фамилия</label><br>
+  <input type="text" name="surname" value=""><br>
+  <br>
+
+  <label>Вод.удостоверения</label><br>
+  <input type="text" name="vod_ud" value=""><br>
+  <br>
+  
+  <label>Дата выдачи</label><br>
+  <input type="text" name="dob" value=""><br>
+  <br>
+  
+  <label>Категории</label><br>
+  <input type="text" name="cat" value=""><br>
+  <br>
+  
+  <button type="submit">Сохранить</button>
+</form>

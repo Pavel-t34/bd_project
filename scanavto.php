36352d4bb313975d0ade35eb2b3bca41cc3ca36e
Pavel-t34 <Paflik12@gmail.com>
2016-12-11 20:53:12 +0800

    Update scanavto.php

--- scanavto.php
+++ scanavto.php
@@ -1,13 +1,34 @@
 <?
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=gibdd60', 'gibdd60', 'Ebhugre0');
+$students = $pdo->query('SELECT * FROM `gibdd60`.`car` WHERE `number` = 'A522AA'');
 
-    /*Подключаемся к БД*/
-    $db = mysql_connect('127.0.0.1','gibdd60','Ebhugre0');
-    mysql_select_db('gibdd60', $db);
-    /*Делаем запрос к БД*/
-    $result = mysql_query("SELECT * FROM `gibdd60`.`car` WHERE `number` = '$id'",$db);
-    /*Преобразовываем результат в массив*/
-    $myrow = mysql_fetch_array($result);
-    /*Выводим результат на экран*/
-    echo $myrow['name'];
+
+    echo '<table border="1" cellspacing="0">';
+
+    echo '<tr>';
+    echo '<th>ID</th>';
+    echo '<th>Марка</th>';
+    echo '<th>Модель</th>';
+    echo '<th>Двигатель</th>';
+    echo '<th>Номер</th>';
+    echo '<th>Владелец</th>';
+    echo '<th>Год</th>';
+    echo '<th>VIN</th>';
+    echo '<th>&nbsp;</th>';
+    echo '</tr>';
+
+    foreach ($students as $student)
+    {
+      echo '<tr>';
+      echo '<td>' . $student['id'] . '</td> ' 
+      . '<td>' . $student['brand'] . '</td> ' 
+      . '<td>' . $student['model'] . '</td>'
+      . '<td>' . $student['engine'] . '</td>'
+      . '<td>' . $student['number'] . '</td>'  
+      . '<td>' . $student['owner'] . '</td>'
+      . '<td>' . $student['year'] . '</td>'
+      . '<td>' . $student['vin'] . '</td>'
+      . '<td><a href="/students.php?action=edit&id=' . $student['id'] . '">ред.</a> <a href="/students.php?action=delete&id=' . $student['id'] . '">уд.</a></td>';
+      echo '</tr>';
 
 ?>
